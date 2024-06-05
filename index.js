d4fc2a5fa5d93fee30c89b0a3feb4b0c6bafb387
Shormily <shormilyrais@gmail.com>
2024-06-05 19:15:25 +0600

    first

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = 5000
 
 app.use(cors());
 app.use(express.json());
+
+// jwt token
+function createToken (users) {
+  const token = jwt.sign(
+    {
+    data: 'foobar'
+    }, 'secret', { expiresIn: '7d' });
+  return token;
+}
+
+function verifyToken(req, res, next) {
+  const authtoken = req.headers.authorization
+  console.log(authtoken);
+  
+}
 
 // MongoDb
 
@@ -31,13 +48,61 @@ async function run() {
     const usersCollection =usersDB.collection("usersCollection")
     const recipesCollection = userDB.collection("recipes_collection");
     // Mongo emil users
-    app.post('/users', async (req, res) => {
+    app.post('/users', verifyToken, async (req, res) => {
       const users = req.body;
-      const result = await usersCollection.insertOne(users);
-      res.send(result);
+      const token = createToken(users);
+      console.log(token);
+      const isUserExist = await usersCollection.findOne({ email: users?.email });
+      if (isUserExist?._id) {
+        return res.send({
+          status: 'success',
+          message: 'Login successfully',
+          token:token
+        })
+      }
+      console.log(isUserExist);
+      await usersCollection.insertOne(users);
+      res.send(token);
     })
+
+    // Get all users endpoint
+    app.get('/users', async (req, res) => {
+      try {
+        const users = await usersCollection.find().toArray();
+        res.status(200).send(users);
+      } catch (error) {
+        res.status(500).send({ error: 'Failed to fetch users' });
+      }
+    });
+
+     // Get userprofilEdit message 
+     app.get('/users/get/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await usersCollection.findOne({
+        _id: new ObjectId(id)
+      }, );
+      res.send(result)
+    });
+    // Get userprofilEdit message End
+    app.patch('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const userData = req.body;
+      
+      const result = await usersCollection.updateOne({ email }, { $set: userData }, { upsert: true });
+      console.log(result)
+      res.send(result )
+    });
+    // Get all users endpoint
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send(result )
+    });
    
     // get add product
+
+   
 
      app.post("/recipes", async (req, res) => {
        const recipesData = req.body;
